2e8d754a964f52bc210f82b97c8ffaaac2e95ae9
Antonio Facciuti <facciuti.anto@gmail.com>
2025-10-13 13:13:32 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ export default function App() {
         scrollLockRef.current = false;
       }, 900); // tempo di blocco dopo ogni scroll
     }
-  });
+  }, [activeSections, currentSection]);
 
   useEffect(() => {
     window.addEventListener("wheel", handleWheel, { passive: false });
