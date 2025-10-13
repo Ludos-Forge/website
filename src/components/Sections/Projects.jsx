10634ee7ca97200e8daa80c8e75d4e18a885d06e
Antonio Facciuti <facciuti.anto@gmail.com>
2025-10-13 15:41:22 +0200

    h-full to h-screen

--- src/components/Sections/Projects.jsx
+++ src/components/Sections/Projects.jsx
@@ -5,7 +5,7 @@ export default function Projects({ isMobile, isBlack }) {
   return (
     <div className={`px-5 py-10 ${!isMobile ? "my-24 mx-48" : ""}`}>
       <h2 className={`text-4xl md:text-5xl mb-6 ${isBlack ? "text-white" : "text-black"}`}>Projects</h2>
-      <div className="h-full overflow-auto">
+      <div className="h-screen overflow-auto">
         <ProjectTimeline />
       </div>
     </div>
